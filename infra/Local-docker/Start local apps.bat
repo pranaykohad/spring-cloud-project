c3f8f031ca5148e4d01abc06aa042e00a350a946
pranay_kohad <prakohad@gmail.com>
2024-11-25 19:49:41 +0530

    notification service added locally

--- infra/Local-docker/Start local apps.bat
+++ infra/Local-docker/Start local apps.bat
@@ -2,4 +2,4 @@
 start cmd /k "java -jar "../../api-gateway/target/api-gateway-0.0.1-SNAPSHOT.jar""
 start cmd /k "java -jar "../../customer-service/target/user-service-0.0.1-SNAPSHOT.jar""
 start cmd /k "java -jar "../../event-service/target/event-service-0.0.1-SNAPSHOT.jar""
-
+start cmd /k "java -jar "../../notification-service/target/notification-service-0.0.1-SNAPSHOT.jar""
