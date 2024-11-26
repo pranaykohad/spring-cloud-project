a63a58bdf86e3fb5a561efc64f825472328722bf
pranay_kohad <prakohad@gmail.com>
2024-11-26 22:12:41 +0530

    image name correction

--- infra/Build docker images/Build and push event-service image.bat
+++ infra/Build docker images/Build and push event-service image.bat
@@ -1,2 +1,2 @@
 @echo off
-start cmd /k "cd ../../customer-service && mvn clean install && mvnw spring-boot:build-image"
+start cmd /k "cd ../../event-service && mvn clean install && mvnw spring-boot:build-image"
