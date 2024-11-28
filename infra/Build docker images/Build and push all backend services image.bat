e7313da656e7272c1d8d09a21a39c2e365b2c717
pranay_kohad <prakohad@gmail.com>
2024-11-28 12:29:41 +0530

    customer changes to user

--- infra/Build docker images/Build and push all backend services image.bat
+++ infra/Build docker images/Build and push all backend services image.bat
@@ -1,5 +1,5 @@
 @echo off
 start cmd /k "cd ../../api-gateway && mvn clean install && mvnw spring-boot:build-image"
-start cmd /k "cd ../../customer-service && mvn clean install && mvnw spring-boot:build-image"
+start cmd /k "cd ../../user-service && mvn clean install && mvnw spring-boot:build-image"
 start cmd /k "cd ../../event-service && mvn clean install && mvnw spring-boot:build-image"
 start cmd /k "cd ../../notification-service && mvn clean install && mvnw spring-boot:build-image"
--- infra/Build docker images/Build and push user-service image.bat
+++ infra/Build docker images/Build and push user-service image.bat
@@ -1,2 +1,2 @@
 @echo off
-start cmd /k "cd ../../customer-service && mvn clean install && mvnw spring-boot:build-image"
+start cmd /k "cd ../../user-service && mvn clean install && mvnw spring-boot:build-image"
