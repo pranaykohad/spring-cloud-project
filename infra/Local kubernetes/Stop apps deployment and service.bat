c17a735933e3d8c2c2c51839b21c441718e6a005
pranay_kohad <prakohad@gmail.com>
2024-12-16 12:19:14 +0530

    kubernetse files updated

--- infra/Local kubernetes/Stop apps deployment and service.bat
+++ infra/Local kubernetes/Stop apps deployment and service.bat
@@ -1,2 +1,2 @@
 @echo off
-start cmd /k "kubectl delete deploy api-gateway user-service event-service angular-app && kubectl delete svc api-gateway user-service event-service angular-app"
+start cmd /k "kubectl delete deploy api-gateway user-service event-service admin-dashboard && kubectl delete svc api-gateway user-service event-service admin-dashboard"
