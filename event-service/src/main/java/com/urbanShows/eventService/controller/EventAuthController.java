44e02e8204aeb7f785955fb1b07bf963b704e88f
pranay_kohad <prakohad@gmail.com>
2025-01-01 13:34:50 +0530

    refactoring

--- event-service/src/main/java/com/urbanShows/eventService/controller/EventAuthController.java
+++ event-service/src/main/java/com/urbanShows/eventService/controller/EventAuthController.java
@@ -1,27 +1,12 @@
 package com.urbanShows.eventService.controller;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.urbanShows.eventService.dto.UserInfoDto;
-import com.urbanShows.eventService.mapper.GenericMapper;
-import com.urbanShows.eventService.security.dto.UserLoginDto;
-import com.urbanShows.eventService.security.dto.UserResponseDto;
-import com.urbanShows.eventService.security.dto.UserSigninDto;
-import com.urbanShows.eventService.security.exception.AccessDeniedException;
 import com.urbanShows.eventService.security.service.JwtService;
-import com.urbanShows.eventService.security.service.UserService;
 
-import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -29,10 +14,8 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class EventAuthController {
 
-	private final UserService userService;
 	private final JwtService jwtService;
 	private final ModelMapper modelMapper;
 	private final AuthenticationManager authenticationManager;
-
 
 }
