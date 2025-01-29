9422a0abd31f0a934d35132ff602f82a0fb852c9
pranay_kohad <prakohad@gmail.com>
2025-01-29 11:19:25 +0530

    ingress controller and notification service added

--- infra/Local kubernetes/Start apps deployment and service.bat
+++ infra/Local kubernetes/Start apps deployment and service.bat
@@ -1,2 +1,2 @@
 @echo off
-start cmd /k "kubectl apply -f application-deploy-service.yaml && kubectl get all"
+start cmd /k "kubectl apply -f application-deploy-service.yaml && kubectl apply -f ingress-controller.yaml && kubectl get all"
--- infra/Local kubernetes/Stop apps deployment and service.bat
+++ infra/Local kubernetes/Stop apps deployment and service.bat
@@ -1,2 +1,2 @@
 @echo off
-start cmd /k "kubectl delete deploy api-gateway user-service event-service admin-dashboard && kubectl delete svc api-gateway user-service event-service admin-dashboard"
+start cmd /k "kubectl delete deploy api-gateway user-service event-service notification-service admin-dashboard && kubectl delete svc api-gateway user-service event-service notification-service admin-dashboard"
