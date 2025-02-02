7fc41488085ba6146b908067852cdc54ac5a310a
pranay_kohad <prakohad@gmail.com>
2025-02-02 11:23:39 +0530

    angular bulid for prod

--- infra/Build docker images/Build and push admin-dashboard image.bat
+++ infra/Build docker images/Build and push admin-dashboard image.bat
@@ -1,2 +1,2 @@
 @echo off
-start cmd /k "cd ../../admin-dashboard && docker build -t admin-dashboard:v1 . && docker tag admin-dashboard:v1 prakohad/admin-dashboard:v1 && docker push prakohad/admin-dashboard:v1"
+start cmd /k "cd ../../admin-dashboard && docker build -f Docker.prod -t admin-dashboard:v1 . && docker tag admin-dashboard:v1 prakohad/admin-dashboard:v1 && docker push prakohad/admin-dashboard:v1"
